29f8c912fe92d423ccf0600b43ddffab86693dd4
RAUL MONTEIRO DE SOUSA CARVALHO <joaomisterioso5199@gmail.com>
2023-05-08 09:06:33 -0300

    criação dos metodos da calculadora

--- methods.py
+++ methods.py
@@ -5,3 +5,16 @@ def area_of_rectangle(width, height):
 def perimeter_of_rectangle(width, height):
     perimeter = 2 * (width + height)
     return perimeter
+
+def subtraction(numero1, numero2):
+    return numero1 - numero2
+
+def addition(numero1, numero2):
+    return numero1 + numero2
+
+def mult(numero1, numero2):
+    return numero1 * numero2
+
+def divide(numero1, numero2):
+    return numero1 / numero2
+
--- test_methods.py
+++ test_methods.py
@@ -21,3 +21,47 @@ def test_perimeter():
     
     # then the perimeter should be 14
     assert output == 14
+
+def test_sub():
+    # given two numbers
+    numero1 = 2
+    numero2 = 5
+
+    #when we calculate subtraction
+    output = methods.subtraction(numero1, numero2)
+
+    #then show the subtraction
+    assert output == -3
+
+def test_add():
+    # given two numbers
+    numero1 = 2
+    numero2 = 5
+
+    #when we calculate addition
+    output = methods.addition(numero1, numero2)
+    #then show the addition
+    assert output == 7
+
+def test_mult():
+    # given two numbers
+    numero1 = 2
+    numero2 = 5
+
+    #when we calculate multiplication
+    output = methods.mult(numero1, numero2)
+
+    #then show the multiplication
+    assert output == 10
+
+def test_divide(numero1 = 2, numero2 = 5):
+    # given two numbers
+    numero1 = 2
+    numero2 = 5
+
+    #when we calculate multiplication
+    output = methods.divide(numero1, numero2)
+
+    #then show the multiplication
+    assert output == 0.4
+        
